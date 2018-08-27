4f8b8c42012658d654605b74d42171c4d84b3722
wen jun <787558290@qq.com>
2018-08-27 16:56:11 +0800

    集成 feign 和 链路监控

--- server-a/src/main/java/org/hwj/servera/ServerAApplication.java
+++ server-a/src/main/java/org/hwj/servera/ServerAApplication.java
@@ -1,11 +1,15 @@
 package org.hwj.servera;
 
+import org.hwj.servera.feign.ServerBAPI;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableFeignClients
 @EnableEurekaClient
 @RestController
 @SpringBootApplication
@@ -18,5 +22,13 @@ public class ServerAApplication {
 	@RequestMapping(value = "/hi")
 	public String hi(){
 		return "helle i am server a";
+	}
+
+	@Autowired
+	ServerBAPI serverBAPI;
+
+	@RequestMapping(value = "/hi-feign")
+	public String hiFeign(){
+		return serverBAPI.hi() + " , caller b";
 	}
 }
